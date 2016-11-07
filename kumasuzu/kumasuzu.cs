43ee7db2f3ea31ca964eaa04a72e7fcbc34590a9
aha-oremata <sekine_y_529@msn.com>
2016-11-07 20:47:23 +0900

    Add Timer

--- kumasuzu/kumasuzu.cs
+++ kumasuzu/kumasuzu.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading;
 using Xamarin.Forms;
 
 namespace kumasuzu
@@ -7,37 +7,52 @@ namespace kumasuzu
 
 	public class App : Application
 	{
-
-		private static string START = "Start!";
-		private static string STOP = "Stop!";
+		private Boolean started = false;
+		private static string START = "開始";
+		private static string STOP = "停止";
 
 		public App()
 		{
 			var soundPlayer = DependencyService.Get<IPlatformSoundPlayer>();
 
-			var button = new Button
-			{
-				Text = START
-			};
+			CancellationTokenSource cancelTokenSource = null;
+			Timer timer;
+
+			var button = createCircularButton();
+			button.Text = START;
 
 			button.Clicked += (sender, e) =>
 			{
-				soundPlayer.playSound();
-				button.Text = button.Text == START ? STOP : START;
+				started = !started;
+				if (started)
+				{
+					cancelTokenSource = new CancellationTokenSource();
+					timer = new Timer((obj) => soundPlayer.playSound(),
+					                  null,
+					                  0,
+					                  5000,
+					                  cancelTokenSource);
+					button.Text = STOP;
+					button.BackgroundColor = Color.White;
+					button.TextColor = Color.Accent;
+				}
+				else {
+					cancelTokenSource.Cancel();
+
+					button.Text = START;
+					button.BackgroundColor = Color.Accent;
+					button.TextColor = Color.White;
+				}
 			};
 
 			// The root page of your application
 			var content = new ContentPage
 			{
-				Title = "kumasuzu",
+				Title = "クマ鈴",
 				Content = new StackLayout
 				{
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						},
 						button
 					}
 				}
@@ -59,6 +74,35 @@ namespace kumasuzu
 		protected override void OnResume()
 		{
 			// Handle when your app resumes
+		}
+
+		private Button createCircularButton()
+		{
+			const int BUTTON_BORDER_WIDTH = 1;
+			const int BUTTON_HEIGHT = 132;
+			const int BUTTON_HEIGHT_WP = 216;
+			const int BUTTON_HALF_HEIGHT = 66;
+			const int BUTTON_HALF_HEIGHT_WP = 108;
+			const int BUTTON_WIDTH = 132;
+			const int BUTTON_WIDTH_WP = 216;
+			const int FONT_SIZE = 27;
+			const int FONT_SIZE_WP = 36;
+
+			return new Button
+			{
+				HorizontalOptions = LayoutOptions.Center,
+		        BackgroundColor = Color.Accent,
+		        BorderColor = Color.Black,
+		        TextColor = Color.White,
+				FontSize = Device.OnPlatform(FONT_SIZE,FONT_SIZE,FONT_SIZE_WP),
+				FontAttributes = FontAttributes.Bold,
+		        BorderWidth = BUTTON_BORDER_WIDTH,
+		        BorderRadius = Device.OnPlatform(BUTTON_HALF_HEIGHT, BUTTON_HALF_HEIGHT, BUTTON_HALF_HEIGHT_WP),
+		        HeightRequest = Device.OnPlatform(BUTTON_HEIGHT, BUTTON_HEIGHT, BUTTON_HEIGHT_WP),
+		        MinimumHeightRequest = Device.OnPlatform(BUTTON_HEIGHT, BUTTON_HEIGHT, BUTTON_HEIGHT_WP),
+		        WidthRequest = Device.OnPlatform(BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH_WP),
+		        MinimumWidthRequest = Device.OnPlatform(BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH_WP)
+			};
 		}
 	}
 }
