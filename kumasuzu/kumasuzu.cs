7ee2ed8ac1e76920cc00ee38f5badcc1b55fa917
aha-oremata <sekine_y_529@msn.com>
2016-11-13 02:13:03 +0900

    button -> image, add applicatioin icon

--- kumasuzu/kumasuzu.cs
+++ kumasuzu/kumasuzu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace kumasuzu
@@ -8,8 +9,8 @@ namespace kumasuzu
 	public class App : Application
 	{
 		private Boolean started = false;
-		private static string START = "開始";
-		private static string STOP = "停止";
+		private static string START_IMAGE_PATH = "kumasuzu.images.kumasuzu_startButton.png";
+		private static string STOP_IMAGE_PATH = "kumasuzu.images.kumasuzu_stopButton.png";
 
 		public App()
 		{
@@ -18,10 +19,14 @@ namespace kumasuzu
 			CancellationTokenSource cancelTokenSource = null;
 			Timer timer;
 
-			var button = createCircularButton();
-			button.Text = START;
+			var image = new Image
+			{
+				Aspect = Aspect.AspectFit,
+				Source = ImageSource.FromResource(START_IMAGE_PATH)
+			};
 
-			button.Clicked += (sender, e) =>
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.Tapped += async (sender, e) =>
 			{
 				started = !started;
 				if (started)
@@ -29,21 +34,26 @@ namespace kumasuzu
 					cancelTokenSource = new CancellationTokenSource();
 					timer = new Timer((obj) => soundPlayer.playSound(),
 					                  null,
-					                  0,
-					                  5000,
-					                  cancelTokenSource);
-					button.Text = STOP;
-					button.BackgroundColor = Color.White;
-					button.TextColor = Color.Accent;
+									  0,
+									  5000,
+									  cancelTokenSource);
+
+					image.Opacity = 0.5;
+					await Task.Delay(150);
+					image.Opacity = 1.0;
+					image.Source = ImageSource.FromResource(STOP_IMAGE_PATH);
 				}
 				else {
 					cancelTokenSource.Cancel();
 
-					button.Text = START;
-					button.BackgroundColor = Color.Accent;
-					button.TextColor = Color.White;
+					image.Opacity = 0.5;
+					await Task.Delay(150);
+					image.Opacity = 1.0;
+					image.Source = ImageSource.FromResource(START_IMAGE_PATH);
 				}
 			};
+
+			image.GestureRecognizers.Add(tapGestureRecognizer);
 
 			// The root page of your application
 			var content = new ContentPage
@@ -53,7 +63,7 @@ namespace kumasuzu
 				{
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
-						button
+						image
 					}
 				}
 			};
@@ -74,35 +84,6 @@ namespace kumasuzu
 		protected override void OnResume()
 		{
 			// Handle when your app resumes
-		}
-
-		private Button createCircularButton()
-		{
-			const int BUTTON_BORDER_WIDTH = 1;
-			const int BUTTON_HEIGHT = 132;
-			const int BUTTON_HEIGHT_WP = 216;
-			const int BUTTON_HALF_HEIGHT = 66;
-			const int BUTTON_HALF_HEIGHT_WP = 108;
-			const int BUTTON_WIDTH = 132;
-			const int BUTTON_WIDTH_WP = 216;
-			const int FONT_SIZE = 27;
-			const int FONT_SIZE_WP = 36;
-
-			return new Button
-			{
-				HorizontalOptions = LayoutOptions.Center,
-		        BackgroundColor = Color.Accent,
-		        BorderColor = Color.Black,
-		        TextColor = Color.White,
-				FontSize = Device.OnPlatform(FONT_SIZE,FONT_SIZE,FONT_SIZE_WP),
-				FontAttributes = FontAttributes.Bold,
-		        BorderWidth = BUTTON_BORDER_WIDTH,
-		        BorderRadius = Device.OnPlatform(BUTTON_HALF_HEIGHT, BUTTON_HALF_HEIGHT, BUTTON_HALF_HEIGHT_WP),
-		        HeightRequest = Device.OnPlatform(BUTTON_HEIGHT, BUTTON_HEIGHT, BUTTON_HEIGHT_WP),
-		        MinimumHeightRequest = Device.OnPlatform(BUTTON_HEIGHT, BUTTON_HEIGHT, BUTTON_HEIGHT_WP),
-		        WidthRequest = Device.OnPlatform(BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH_WP),
-		        MinimumWidthRequest = Device.OnPlatform(BUTTON_WIDTH, BUTTON_WIDTH, BUTTON_WIDTH_WP)
-			};
 		}
 	}
 }
